8942522e7fe6adc13f5aea906bd50239e603d5d2
Sai Kulkarni <saikulkarni0@gmail.com>
2017-11-10 05:31:27 -0500

    Initial commit with problem definition

---
+++ pokerhand.py
@@ -0,0 +1,28 @@
+'''
+Author: Sai Kulkarni
+Email: saikulkarni0@gmail.com
+    
+This programs returns the optimal card for each player using Texas Hold'm rules
+
+Under Texas Hold'em poker rules, each player hold 2 cards in their hand and as 
+the game progresses, 5 additional cards are revealed on the table and shared by 
+all players. Using the 5 table cards and 2 cards in his hands, each player can 
+form an optimal hand of 5 cards.
+https://en.wikipedia.org/wiki/Texas_hold_'em
+
+The ranking of hands in Texas Hold'em Ace-High rules is as follows:
+
+1. Straight flush      5 Consecutive values of same suite 
+2. Four of a Kind      4 cards of the same value 
+3. Full House          3 Cards of the 
+4. Flush               5 cards of the same suite
+5. Straight            5 cards of any suite with consecutive values
+6. Three of a kind     3 cards of same value
+7. Two pair            2 pairs of 2 cards with same value
+8. One pair            2 cards with same value
+9. High card           Using highest value card for comparison, in absence other hands
+
+'''
+
+
+
